20800579c90799f027cbd7ba1af0af7a15d5ebc2
Jonathas Cavalcante <jbc@icomp.ufam.edu.br>
2016-08-11 18:26:42 -0400

    Meu primeiro script PHP slide 47 aula6_php.pdf

---
+++ labWeb30-07-16/imprime.php
@@ -0,0 +1,9 @@
+<?php  
+	
+$nome = $_POST['nome'];
+$sexo = $_POST['sexo'];
+$comentario = $_POST['comentario'];
+
+echo "Nome: " . $nome . " Sexo: ". $sexo . "<br>" . "comentario: " . $comentario;	
+
+?>
