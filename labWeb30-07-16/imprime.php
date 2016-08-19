9c06412ab526a83971bd160a3338e4d4f7a6ba45
Jonathas Cavalcante <jbc@icomp.ufam.edu.br>
2016-08-18 22:22:47 -0400

    Apaguei geral

--- labWeb30-07-16/imprime.php
+++
@@ -1,9 +0,0 @@
-<?php  
-	
-$nome = $_POST['nome'];
-$sexo = $_POST['sexo'];
-$comentario = $_POST['comentario'];
-
-echo "Nome: " . $nome . " Sexo: ". $sexo . "<br>" . "comentario: " . $comentario;	
-
-?>
