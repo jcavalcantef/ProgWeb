172e9f2ea94c8c6effce96542f01ff6960c3ed3a
Jonathas Cavalcante <jbc@icomp.ufam.edu.br>
2016-08-18 22:59:53 -0400

    Segundo exercicio slide 74 aula7_yii2.pdf assunto: controller c view

---
+++ a21353622/questao_02.js
@@ -0,0 +1,12 @@
+function verifica(){
+	var nome  = document.getElementById('nome').value;
+	var email  = document.getElementById('email').value;
+	if(nome == null){
+		alert("Nome Não foi preenchido");
+	}
+		
+	if(email == null){
+		alert("Nome nao foi preenchido");
+	}
+	
+}
